5e4cae4f99f226ccf882b0cbfd8a06dfc5ec9926
kanta <82421244+kanta13024@users.noreply.github.com>
2021-05-15 12:18:27 +0900

    タスクのビューを作成

--- backend/app/Folder.php
+++ backend/app/Folder.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Folder extends Model
 {
-    //
+    // TaskController→
+    // return tasks()
+    // Laravel はフォルダテーブルとタスクテーブルの関連性（リレーション）
+    // を「たどって」、フォルダクラスのインスタンスから紐づくタスククラスのリストを取得
+
+    public function tasks()
+    {
+        return $this->hasMany('App\Task');
+    }
 }
--- backend/app/Http/Controllers/TaskController.php
+++ backend/app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Folder;
+use App\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -12,11 +13,21 @@ class TaskController extends Controller
      */
     public function index(int $id)
     {
+        // すべてのフォルダーを取得する
         $folders = Folder::all();
+
+        // 選ばれたフォルダを取得する
+        $current_folder = Folder::find($id);
+
+        // 選ばれたフォルダに紐づくタスクを取得する
+        $tasks = $current_folder->tasks()->get(); //Folder.phpのtasks()メソッドを使用
 
         return view('tasks/index',[
             'folders' => $folders,
-            'current_folder_id' => $id,
+            'current_folder_id' => $current_folder->id,
+            'tasks' => $tasks,
         ]);
+
     }
+
 }
